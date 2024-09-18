6cc97e9792fbe820f904f3b4e3ec3b5944d2d447
Val <val.montesclaros@netsells.co.uk>
2024-09-18 20:26:29 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
+| sample code
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
