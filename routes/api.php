1b9b76a0ba9d3093582b8fe2c1b16452accbd120
Val <val.montesclaros@netsells.co.uk>
2024-09-19 20:20:25 +0800

    Should not allow main to push

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-| sample code
- another one
-  3rd line
-  4th line
+ should not allow
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
