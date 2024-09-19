8f415f3007a63b6e021de84c4d2c26d00fe0c619
Val <val.montesclaros@netsells.co.uk>
2024-09-19 20:49:02 +0800

    Add line for develop

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 added line for develop
+should not allow for develop
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
