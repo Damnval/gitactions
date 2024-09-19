55b9c95a66673cc72a0972e0d33efcdb0df0f697
Val <val.montesclaros@netsells.co.uk>
2024-09-19 20:38:38 +0800

    Add new comment

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
- should not allow
+ should not allow to push
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
