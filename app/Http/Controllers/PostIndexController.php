96a670fb4841f4676b72b5e43b1ee458cc7a6566
Val <val.montesclaros@netsells.co.uk>
2024-09-26 21:00:31 +0800

    again

--- app/Http/Controllers/PostIndexController.php
+++ app/Http/Controllers/PostIndexController.php
@@ -6,7 +6,7 @@ class PostIndexController extends Controller
 {
     public function __invoke(): bool
     {
-        // uncessary comment this fuck
+        // uncessary comment this fuck agian
         if ( !'val'==='val') {
             return true;
         }
